ee78a36b531792886e66998d3b86962dad8589ed
Apprenant 08 <apprenant08@macbook-pro-de-apprenant.home>
2022-05-26 10:30:44 +0200

    emi

---
+++ GitLearn/EmiView.swift
@@ -0,0 +1,20 @@
+//
+//  EmiView.swift
+//  GitLearn
+//
+//  Created by Apprenant 08 on 26/05/2022.
+//
+
+import SwiftUI
+
+struct EmiView: View {
+    var body: some View {
+        Text("emi")
+    }
+}
+
+struct EmiView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmiView()
+    }
+}
