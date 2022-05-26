a51a675345620275778873644724975ae080245b
Apprenant 08 <apprenant08@macbook-pro-de-apprenant.home>
2022-05-26 10:32:11 +0200

    rico

---
+++ GitLearn/RicoView.swift
@@ -0,0 +1,20 @@
+//
+//  RicoView.swift
+//  GitLearn
+//
+//  Created by Apprenant 08 on 26/05/2022.
+//
+
+import SwiftUI
+
+struct RicoView: View {
+    var body: some View {
+        Text("rico")
+    }
+}
+
+struct RicoView_Previews: PreviewProvider {
+    static var previews: some View {
+        RicoView()
+    }
+}
