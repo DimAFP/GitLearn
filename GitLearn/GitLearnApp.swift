13b22e3cb6aebd566fd18df5de5bf10ef428b3c6
Apprenant 08 <apprenant08@macbook-pro-de-apprenant.home>
2022-05-26 10:29:32 +0200

    first

---
+++ GitLearn/GitLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLearnApp.swift
+//  GitLearn
+//
+//  Created by Apprenant 08 on 26/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
